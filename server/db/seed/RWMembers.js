b0cebdf0886ff3ead8af7d837b5035982ffa0f4c
Kate Quinn <katequinn7@gmail.com>
2021-05-10 14:59:26 -0400

    seeding RW data

--- server/db/seed/RWMembers.js
+++ server/db/seed/RWMembers.js
@@ -1,65 +1,65 @@
-const members = [
-  {
-    firstName: 'Melissa V',
-    lastName: 'Veloz',
-    bio: `Just trying to get by...`,
-    genre: `Historical Fiction, Mystery`,
-    imageUrl: `/melv.jpeg`,
-    faveBook: `The Awakening, She’s Come Undone`,
-    favePick: ``,
-  },
-  {
-    firstName: 'Melissa M',
-    lastName: 'Mahon',
-    bio: `Charismatic lover of life, extrovert that is married to an introvert with 2 highly adored spunky children,
-    and a communication expert who couldn't survive this thing called life without the fabulous village around her.`,
-    genre: `Psychological Thriller/ Mystery`,
-    imageUrl: `/melm2.jpg`,
-    faveBook: `The Interpreter of Maladies`,
-    favePick: ``,
-  },
-  {
-    firstName: 'Laura',
-    lastName: 'Yarusavage',
-    bio: `Age 42, enjoys long walks in the woods, is AN EDUCATOR IN THIS TOWN, has 3 douchey kids, and a husband 
-    that talks a whole lot. `,
-    genre: `Fiction/porn`,
-    imageUrl: `/laura.jpeg`,
-    faveBook: `She's Come Undone`,
-    favePick: `American Dirt`,
-  },
+// const members = [
+//   {
+//     firstName: 'Melissa V',
+//     lastName: 'Veloz',
+//     bio: `Just trying to get by...`,
+//     genre: `Historical Fiction, Mystery`,
+//     imageUrl: `/melv.jpeg`,
+//     faveBook: `The Awakening, She’s Come Undone`,
+//     favePick: ``,
+//   },
+//   {
+//     firstName: 'Melissa M',
+//     lastName: 'Mahon',
+//     bio: `Charismatic lover of life, extrovert that is married to an introvert with 2 highly adored spunky children,
+//     and a communication expert who couldn't survive this thing called life without the fabulous village around her.`,
+//     genre: `Psychological Thriller/ Mystery`,
+//     imageUrl: `/melm2.jpg`,
+//     faveBook: `The Interpreter of Maladies`,
+//     favePick: ``,
+//   },
+//   {
+//     firstName: 'Laura',
+//     lastName: 'Yarusavage',
+//     bio: `Age 42, enjoys long walks in the woods, is AN EDUCATOR IN THIS TOWN, has 3 douchey kids, and a husband
+//     that talks a whole lot. `,
+//     genre: `Fiction/porn`,
+//     imageUrl: `/laura.jpeg`,
+//     faveBook: `She's Come Undone`,
+//     favePick: `American Dirt`,
+//   },
 
-  {
-    firstName: 'Courtney',
-    lastName: 'Marsh',
-    bio: `30 something Long Islander turned Nutmeger. Seal selling, Exercise crazied, boy mom, 
-    who volunteers too much, loves red wine, margaritas, and laughing her ass off. `,
-    genre: `Historical Fiction`,
-    imageUrl: `/courtney.jpeg`,
-    faveBook: `American Dirt`,
-    favePick: ``,
-  },
-  {
-    firstName: 'Kate',
-    lastName: 'Quinn',
-    bio: `Single Dad to two amazing kids, lover of life and all things Jo-Jo Siwa.  `,
-    genre: `Twilight Fan Fiction`,
-    imageUrl: ``,
-    faveBook: `Are You There, God? It's Me, Margaret.`,
-    favePick: `Whichever one I picked`,
-  },
-  {
-    firstName: 'Sarah',
-    lastName: 'Voris',
-    bio: `Mom to 2 boys who would rather be with their devices than their family.  Employee extraordinaire 
-    who works relentlessly to make sure the job gets done.  Pinot Noir, massages and spa pedicures put me in my peaceful place 
-    (seriously missing the massages and pedi's this week).  Beginning to appreciate the outdoors - biking, hiking, camping and all.  
-    Reader of 1 author who joined book club to experience more and gain back some portion of my life with women who make me laugh! `,
-    genre: `crime (fiction)`,
-    imageUrl: `/sarah.jpg`,
-    faveBook: `Anything James Patterson - Alex Cross or Michael Bennett`,
-    favePick: ``,
-  },
-];
+//   {
+//     firstName: 'Courtney',
+//     lastName: 'Marsh',
+//     bio: `30 something Long Islander turned Nutmeger. Seal selling, Exercise crazied, boy mom,
+//     who volunteers too much, loves red wine, margaritas, and laughing her ass off. `,
+//     genre: `Historical Fiction`,
+//     imageUrl: `/courtney.jpeg`,
+//     faveBook: `American Dirt`,
+//     favePick: ``,
+//   },
+//   {
+//     firstName: 'Kate',
+//     lastName: 'Quinn',
+//     bio: `Single Dad to two amazing kids, lover of life and all things Jo-Jo Siwa.  `,
+//     genre: `Twilight Fan Fiction`,
+//     imageUrl: ``,
+//     faveBook: `Are You There, God? It's Me, Margaret.`,
+//     favePick: `Whichever one I picked`,
+//   },
+//   {
+//     firstName: 'Sarah',
+//     lastName: 'Voris',
+//     bio: `Mom to 2 boys who would rather be with their devices than their family.  Employee extraordinaire
+//     who works relentlessly to make sure the job gets done.  Pinot Noir, massages and spa pedicures put me in my peaceful place
+//     (seriously missing the massages and pedi's this week).  Beginning to appreciate the outdoors - biking, hiking, camping and all.
+//     Reader of 1 author who joined book club to experience more and gain back some portion of my life with women who make me laugh! `,
+//     genre: `crime (fiction)`,
+//     imageUrl: `/sarah.jpg`,
+//     faveBook: `Anything James Patterson - Alex Cross or Michael Bennett`,
+//     favePick: ``,
+//   },
+// ];
 
-module.exports = members;
+// module.exports = members;
---
+++ server/db/seed/readsweedseed.js
@@ -0,0 +1,93 @@
+const members = [
+  {
+    firstName: 'Melissa V',
+    email: 'melissaveloz4197',
+    lastName: 'Veloz',
+    password: 'weed',
+    bio: `Just trying to get by...`,
+    genre: `Historical Fiction, Mystery`,
+    imageUrl: `/melv.jpeg`,
+    faveBook: `The Awakening, She’s Come Undone`,
+    favePick: ``,
+  },
+  {
+    firstName: 'Melissa M',
+    lastName: 'Mahon',
+    password: 'weed',
+    email: 'melissamahon33@gmail.com',
+    bio: `Charismatic lover of life, extrovert that is married to an introvert with 2 highly adored spunky children,
+      and a communication expert who couldn't survive this thing called life without the fabulous village around her.`,
+    genre: `Psychological Thriller/ Mystery`,
+    imageUrl: `/melm2.jpg`,
+    faveBook: `The Interpreter of Maladies`,
+    favePick: ``,
+  },
+  {
+    firstName: 'Laura',
+    email: 'lyarusavage',
+    lastName: 'Yarusavage',
+    password: 'weed',
+    bio: `Age 42, enjoys long walks in the woods, is AN EDUCATOR IN THIS TOWN, has 3 douchey kids, and a husband 
+      that talks a whole lot. `,
+    genre: `Fiction/porn`,
+    imageUrl: `/laura.jpeg`,
+    faveBook: `She's Come Undone`,
+    favePick: `American Dirt`,
+  },
+
+  {
+    firstName: 'Courtney',
+    lastName: 'Marsh',
+    password: 'weed',
+    email: 'courtney.j.marsh@gmail.com',
+    bio: `30 something Long Islander turned Nutmeger. Seal selling, Exercise crazied, boy mom, 
+      who volunteers too much, loves red wine, margaritas, and laughing her ass off. `,
+    genre: `Historical Fiction`,
+    imageUrl: `/courtney.jpeg`,
+    faveBook: `American Dirt`,
+    favePick: ``,
+  },
+  {
+    firstName: 'Kate',
+    lastName: 'Quinn',
+    email: 'katequinn7@gmail.com',
+    password: 'weed',
+    bio: `Single Dad to two amazing kids, lover of life and all things Jo-Jo Siwa.  `,
+    genre: `Twilight Fan Fiction`,
+    imageUrl: ``,
+    faveBook: `Are You There, God? It's Me, Margaret.`,
+    favePick: `Whichever one I picked`,
+  },
+  {
+    firstName: 'Sarah',
+    lastName: 'Voris',
+    password: 'weed',
+    email: 'spurcell922@yahoo.com',
+    bio: `Mom to 2 boys who would rather be with their devices than their family.  Employee extraordinaire 
+      who works relentlessly to make sure the job gets done.  Pinot Noir, massages and spa pedicures put me in my peaceful place 
+      (seriously missing the massages and pedi's this week).  Beginning to appreciate the outdoors - biking, hiking, camping and all.  
+      Reader of 1 author who joined book club to experience more and gain back some portion of my life with women who make me laugh! `,
+    genre: `crime (fiction)`,
+    imageUrl: `/sarah.jpg`,
+    faveBook: `Anything James Patterson - Alex Cross or Michael Bennett`,
+    favePick: ``,
+  },
+];
+
+const rwBooks = [
+  { gbId: `PmpfDwAAQBAJ`, number: 1 },
+  { gbId: 'RidvDwAAQBAJ', number: 2 },
+  { gbId: '9-GCDwAAQBAJ', number: 3 },
+  { gbId: `wKFaDwAAQBAJ`, number: 4 },
+  { gbId: `ey-BDwAAQBAJ`, number: 5 },
+  { gbId: `FkiSDwAAQBAJ`, number: 6 },
+  { gbId: `gt7EQgH8-b4C`, number: 7 },
+  { gbId: `dAzJCwAAQBAJ`, number: 8 },
+  { gbId: `rQumDwAAQBAJ`, number: 9 },
+  { gbId: 'SUdfDwAAQBAJ', number: 10 },
+  { gbId: 'nNjTDwAAQBAJ', number: 11 },
+  { gbId: 'wq7eDwAAQBAJ', number: 12 },
+  { gbId: 'xmC9DwAAQBAJ', number: 13, isCurrent: true },
+];
+
+module.exports = { members, rwBooks };
