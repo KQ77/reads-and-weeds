108abd63b35df976418f71450e54025c2c8fda2f
Kate Quinn <katequinn7@gmail.com>
2022-10-26 12:54:45 -0400

    add link  from modal in nav + change menu order

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -26,125 +26,131 @@ const _Nav = (props) => {
   const { memberClubs } = props;
 
   return (
-    <div id="navbar-container">
+    <>
+      {' '}
       <Burger {...props} />
-      <Navbar id="navbar" expand="lg">
-        <Container>
-          {/* <Nav> */}
-          {/* <Burger {...props} /> */}
-          {/* </Nav> */}
-          {/* <Navbar.Brand style={{ color: '#c1c2c9' }} href="/"> */}
-          <Navbar.Brand style={{ color: 'white' }} href="/">
-            Q-Books
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          {/* <Navbar.Collapse id="basic-navbar-nav"> */}
-          <Nav>
-            <NavDropdown
-              id="nav-dropdown"
-              title={
-                <span style={{ color: 'white' }}>
-                  <img
-                    style={{
-                      marginRight: '.3rem',
-                      borderRadius: '100%',
-                      border: '1px solid white',
-                    }}
-                    height="35"
-                    width="35"
-                    src={props.auth.imageUrl || '/images/defaultProfile.png'}
-                  />
-                  {props.auth.firstName || ''}
-                </span>
-              }
-            >
-              {isLoggedIn ? (
-                <>
-                  <NavDropdown.Item
-                    style={{ color: 'black' }}
-                    href={`/members/${props.auth.id}`}
-                  >
-                    Profile
-                  </NavDropdown.Item>
-                  <NavDropdown.Item
-                    style={{ color: 'black' }}
-                    href={`/members/${props.auth.id}/clubs`}
-                  >
-                    Your Clubs
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item
-                    onClick={() => {
-                      props.logout();
-                    }}
-                  >
-                    Log Out
-                  </NavDropdown.Item>
-                </>
-              ) : (
-                ''
-              )}
-              {!isLoggedIn ? (
-                <>
-                  <NavDropdown.Item id="login-link" href={`/login?redirect=`}>
-                    Login
-                  </NavDropdown.Item>
-                  {/* <NavDropdown.Item id="signup-link" href={`/login?redirect=`}>
+      <div id="navbar-container">
+        <Navbar id="navbar" expand="lg">
+          <Container>
+            {/* <Nav> */}
+            {/* <Burger {...props} /> */}
+            {/* </Nav> */}
+            {/* <Navbar.Brand style={{ color: '#c1c2c9' }} href="/"> */}
+            <Navbar.Brand style={{ color: 'white' }} href="/">
+              Q-Books
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            {/* <Navbar.Collapse id="basic-navbar-nav"> */}
+            <Nav>
+              <NavDropdown
+                id="nav-dropdown"
+                title={
+                  <span style={{ color: 'white' }}>
+                    <img
+                      style={{
+                        marginRight: '.3rem',
+                        borderRadius: '100%',
+                        border: '1px solid white',
+                      }}
+                      height="35"
+                      width="35"
+                      src={props.auth.imageUrl || '/images/defaultProfile.png'}
+                    />
+                    {props.auth.firstName || ''}
+                  </span>
+                }
+              >
+                {isLoggedIn ? (
+                  <>
+                    <NavDropdown.Item
+                      style={{ color: 'black' }}
+                      href={`/members/${props.auth.id}`}
+                    >
+                      Profile
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                      style={{ color: 'black' }}
+                      href={`/members/${props.auth.id}/clubs`}
+                    >
+                      Your Clubs
+                    </NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item
+                      onClick={() => {
+                        props.logout();
+                      }}
+                    >
+                      Log Out
+                    </NavDropdown.Item>
+                  </>
+                ) : (
+                  ''
+                )}
+                {!isLoggedIn ? (
+                  <>
+                    <NavDropdown.Item id="login-link" href={`/login?redirect=`}>
+                      Login
+                    </NavDropdown.Item>
+                    {/* <NavDropdown.Item id="signup-link" href={`/login?redirect=`}>
                   Register
                 </NavDropdown.Item> */}
-                  <NavDropdown.Item
-                    id="signup-link"
-                    href={`/register?redirect=`}
+                    <NavDropdown.Item
+                      id="signup-link"
+                      href={`/register?redirect=`}
+                    >
+                      Register
+                    </NavDropdown.Item>
+                  </>
+                ) : (
+                  ''
+                )}
+              </NavDropdown>
+            </Nav>
+            <Navbar.Text onClick={() => setOpen(true)} id="navbar-clubs">
+              Your Clubs
+            </Navbar.Text>
+          </Container>
+          {/* </Navbar.Collapse> */}
+        </Navbar>
+        <Modal show={open} onHide={() => setOpen(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Your Clubs</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {memberClubs.length ? (
+              memberClubs.map((club, idx) => (
+                <div key={idx}>
+                  <Link
+                    onClick={() => setOpen(false)}
+                    to={`/bookclubs/${club.id}`}
                   >
-                    Register
-                  </NavDropdown.Item>
-                </>
-              ) : (
-                ''
-              )}
-            </NavDropdown>
-          </Nav>
-          <Navbar.Text onClick={() => setOpen(true)} id="navbar-clubs">
-            Your Clubs
-          </Navbar.Text>
-        </Container>
-        {/* </Navbar.Collapse> */}
-      </Navbar>
-      <Modal show={open} onHide={() => setOpen(false)}>
-        <Modal.Header closeButton>
-          <Modal.Title>Your Clubs</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          {memberClubs.length ? (
-            memberClubs.map((club, idx) => (
-              <div key={idx}>
-                <Link
-                  onClick={() => setOpen(false)}
-                  to={`/bookclubs/${club.id}`}
-                >
-                  <img
-                    style={{
-                      marginRight: '2rem',
-                      borderRadius: '100%',
-                      height: '3rem',
-                      width: '3rem',
-                    }}
-                    src={club.displayImage}
-                  />
-                  {club.name}
+                    <img
+                      style={{
+                        marginRight: '2rem',
+                        borderRadius: '100%',
+                        height: '3rem',
+                        width: '3rem',
+                      }}
+                      src={club.displayImage}
+                    />
+                    {club.name}
+                  </Link>
+                  <hr />
+                </div>
+              ))
+            ) : (
+              <div>
+                <p>You are not a member of any club at this time</p>
+                <Link to="/create">
+                  <center>
+                    <button>Create Club</button>
+                  </center>
                 </Link>
-                <hr />
               </div>
-            ))
-          ) : (
-            <div>
-              You are not a member of any club at this time
-              <button>Create Club</button>
-            </div>
-          )}
-        </Modal.Body>
-      </Modal>
-      {/* <InputGroup className="justify-content-center" id="searchbar">
+            )}
+          </Modal.Body>
+        </Modal>
+        {/* <InputGroup className="justify-content-center" id="searchbar">
         <InputGroup.Prepend>
           <InputGroup.Text>
             <img height="20px" src="/images/search_icon.png" />
@@ -163,7 +169,8 @@ const _Nav = (props) => {
           Search
         </Button>
       </InputGroup> */}
-    </div>
+      </div>
+    </>
   );
 };
 
